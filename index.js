b3255cce9ac4141c56439ff15ed5ce44a3407509
bharat-yosa <bharat.joshi@appsontechnologies.in>
2024-03-07 14:01:29 +0530

    validate payload

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require("express")
-const app = express();
+const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
+const app = express();
 app.use(express.json())
 
 app.post("/todos", (req, res) => {
-    console.log(req.body)
+    const createPayload = req.body
+    const parsepayload = createTodo.safeParse(createPayload)
+    if(!parsepayload.success)
+    {
+        res.status(411).json({
+            msg:"you sent the wrong input "
+        })
+        return
+    }
+
 })
 
 app.get("/todos", (req, res) => {
@@ -12,5 +22,14 @@ app.get("/todos", (req, res) => {
 })
 
 app.put("/completed", (req, res) => {
-    console.log(req.body)
+    const updatePayload = req.body
+    const parsepayload = createTodo.safeParse(updatePayload)
+    if(!parsepayload.success)
+    {
+        res.status(411).json({
+            msg:"you sent the wrong input "
+        })
+        return
+    }
+
 })
